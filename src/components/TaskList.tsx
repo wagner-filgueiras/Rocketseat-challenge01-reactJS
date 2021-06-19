2b928e33b8e58b20e4ee330c14a16fcdc2597a81
Wagner Antonelli <wagner.a.filgueiras@gmail.com>
2021-06-18 23:31:20 -0600

    Challenge Completed

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,38 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) return;
+
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    setTasks(oldState => [...oldState, newTask]);
+    setNewTaskTitle('');
+
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map(task => task.id === id ? {
+      ...task, 
+      isComplete: !task.isComplete
+    } : task);
+
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    // em vez de remover a task pelo id ele retorna um array filtrando pelo id desejadores
+    // apenas o id desejado nao ira aparecer e entao atualizamos o estado das tasks sem aquela task id.
+    // ou seja ele mantem as tasks q tem o id diferente daquele que foi clicado.
+    const filteredTasks = tasks.filter(task => task.id !== id);
+
+    setTasks(filteredTasks);
+
   }
 
   return (
